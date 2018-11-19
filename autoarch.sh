67429373849a8a51427f7aedc2e871e53e6257d6
Ahmed Alshakh <alshakh@users.noreply.github.com>
2018-11-19 07:22:15 +0300

    Update autoarch.sh

--- autoarch.sh
+++ autoarch.sh
@@ -1,8 +1,1 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="refresh" content="0; url=https://gist.githubusercontent.com/alshakh/3e121b45b761d4b38cb997e127459bce/raw/e1f3c4cfc9335924c34bb492619e644c3e9ff1fd/script.sh">
-</head>
-<body>
-</body>
-</html>
+https://gist.githubusercontent.com/alshakh/3e121b45b761d4b38cb997e127459bce/raw/e1f3c4cfc9335924c34bb492619e644c3e9ff1fd/script.sh
